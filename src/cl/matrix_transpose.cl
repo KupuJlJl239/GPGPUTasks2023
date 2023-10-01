bf11db73ffedc3ada8f437990153bc4539d34dbb
Kirill Sokolov <kirill141103@yandex.ru>
2023-10-01 12:47:40 +0300

    Сделал транспонирование

--- src/cl/matrix_transpose.cl
+++ src/cl/matrix_transpose.cl
@@ -1,4 +1,31 @@
-__kernel void matrix_transpose(...)
+#ifdef __CLION_IDE__
+    #include <libgpu/opencl/cl/clion_defines.cl>
+    #include <libgpu/opencl/cl/common.cl>
+#endif
+
+#line 6
+
+#define TILE_SIZE 16
+__kernel void matrix_transpose(__global float* in_matrix, __global float* out_matrix, unsigned int X, unsigned int Y)
 {
-    // TODO
+    __local float tile[TILE_SIZE*TILE_SIZE];
+    int local_x = get_local_id(0);
+    int local_y = get_local_id(1);
+    int global_x = get_global_id(0);
+    int global_y = get_global_id(1);
+    int group_x = get_group_id(0);
+    int group_y = get_group_id(1);
+
+// tile[ LOCAL_IDX(local_x, local_y) ], tile[ LOCAL_IDX(local_y, local_x) ] - обращения без bank конфликтов
+#define LOCAL_IDX(x, y) ((x+y)%TILE_SIZE + TILE_SIZE*y)
+
+    // 1) копируем из глобальной памяти
+    tile[ LOCAL_IDX(local_x, local_y) ] = in_matrix[global_x + X*global_y];
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    // 2) записываем результат
+    int out_x = local_x + group_y * TILE_SIZE;
+    int out_y = local_y + group_x * TILE_SIZE;
+    if(out_x < Y && out_y < X)
+        out_matrix[ out_x + Y*out_y] = tile[ LOCAL_IDX(local_y, local_x) ];
 }
--- src/main_matrix_transpose.cpp
+++ src/main_matrix_transpose.cpp
@@ -20,24 +20,24 @@ int main(int argc, char **argv)
     context.activate();
 
     int benchmarkingIters = 10;
-    unsigned int M = 1024;
-    unsigned int K = 1024;
+    unsigned int X = 1024;
+    unsigned int Y = 1024;
 
-    std::vector<float> as(M*K, 0);
-    std::vector<float> as_t(M*K, 0);
+    std::vector<float> as(X * Y, 0);
+    std::vector<float> as_t(X * Y, 0);
 
-    FastRandom r(M+K);
+    FastRandom r(X + Y);
     for (unsigned int i = 0; i < as.size(); ++i) {
         as[i] = r.nextf();
     }
-    std::cout << "Data generated for M=" << M << ", K=" << K << std::endl;
+    std::cout << "Data generated for X=" << X << ", Y=" << Y << std::endl;
 
-    /*
+
     gpu::gpu_mem_32f as_gpu, as_t_gpu;
-    as_gpu.resizeN(M*K);
-    as_t_gpu.resizeN(K*M);
+    as_gpu.resizeN(X * Y);
+    as_t_gpu.resizeN(Y * X);
 
-    as_gpu.writeN(as.data(), M*K);
+    as_gpu.writeN(as.data(), X * Y);
 
     ocl::Kernel matrix_transpose_kernel(matrix_transpose, matrix_transpose_length, "matrix_transpose");
     matrix_transpose_kernel.compile();
@@ -45,36 +45,32 @@ int main(int argc, char **argv)
     {
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            // TODO
-            unsigned int work_group_size = 128;
-            unsigned int global_work_size = ...;
             // Для этой задачи естественнее использовать двухмерный NDRange. Чтобы это сформулировать
             // в терминологии библиотеки - нужно вызвать другую вариацию конструктора WorkSize.
             // В CLion удобно смотреть какие есть вариант аргументов в конструкторах:
             // поставьте каретку редактирования кода внутри скобок конструктора WorkSize -> Ctrl+P -> заметьте что есть 2, 4 и 6 параметров
             // - для 1D, 2D и 3D рабочего пространства соответственно
-            matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size, global_work_size), as_gpu, as_t_gpu, M, K);
+            matrix_transpose_kernel.exec(gpu::WorkSize(16, 16, X, Y), as_gpu, as_t_gpu, X, Y);
 
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << M*K/1000.0/1000.0 / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "GPU: " << X * Y /1000.0/1000.0 / t.lapAvg() << " millions/s" << std::endl;
     }
 
-    as_t_gpu.readN(as_t.data(), M*K);
+    as_t_gpu.readN(as_t.data(), X * Y);
 
     // Проверяем корректность результатов
-    for (int j = 0; j < M; ++j) {
-        for (int i = 0; i < K; ++i) {
-            float a = as[j * K + i];
-            float b = as_t[i * M + j];
+    for (int j = 0; j < X; ++j) {
+        for (int i = 0; i < Y; ++i) {
+            float a = as[j * Y + i];
+            float b = as_t[i * X + j];
             if (a != b) {
                 std::cerr << "Not the same!" << std::endl;
                 return 1;
             }
         }
     }
-    */
 
     return 0;
 }
