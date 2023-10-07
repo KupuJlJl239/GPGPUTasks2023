02dde674123aece11486fa8af87620180edd7b98
Kirill Sokolov <kirill141103@yandex.ru>
2023-10-07 21:28:09 +0300

    Сделал

--- src/cl/merge.cl
+++ src/cl/merge.cl
@@ -1,1 +1,47 @@
+#ifdef __CLION_IDE__
+    #include <libgpu/opencl/cl/clion_defines.cl>
+    #include <libgpu/opencl/cl/common.cl>
+#endif
 
+#line 6
+
+#define uint unsigned int
+
+__kernel void merge(__global float* in, __global float* out, uint step)
+{
+
+#define CHECK_LESS(i, j) in[i] < in[j] || (in[i] == in[j] && i < j)
+
+    const uint n = get_global_size(0);
+    const uint idx = get_global_id(0);
+    const uint size = 1<<step;
+
+    uint nA = idx / size;
+    uint nB = nA % 2 == 0? nA+1:nA-1;
+    uint nres = idx / (2*size);
+    __global float* res = &out[nres*2*size];
+
+    uint countA = idx % size;
+    uint countB; // == ???
+
+    uint p0 = nB * size;
+    uint p1 = p0 + size - 1;
+
+    if(CHECK_LESS(idx, p0)){
+        countB = 0;
+    }
+    else if(CHECK_LESS(p1, idx)){
+        countB = size;
+    }
+    else {
+        while (p1 - p0 > 1) {
+            uint p = (p0 + p1) / 2;
+            if (CHECK_LESS(p, idx))
+                p0 = p;
+            else
+                p1 = p;
+        }
+        countB = p1 - nB * size;
+    }
+    res[countA + countB] = in[idx];
+}
--- src/main_merge.cpp
+++ src/main_merge.cpp
@@ -11,6 +11,7 @@
 #include <stdexcept>
 #include <vector>
 
+#define uint unsigned int
 
 template<typename T>
 void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line) {
@@ -31,9 +32,9 @@ int main(int argc, char **argv) {
     context.activate();
 
     int benchmarkingIters = 10;
-    unsigned int n = 32 * 1024 * 1024;
+    unsigned int n = 1024*1024;
     std::vector<float> as(n, 0);
-    FastRandom r(n);
+    FastRandom r(0);
     for (unsigned int i = 0; i < n; ++i) {
         as[i] = r.nextf();
     }
@@ -48,31 +49,40 @@ int main(int argc, char **argv) {
             t.nextLap();
         }
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "CPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "CPU: " << ((float)n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-    /*
-    gpu::gpu_mem_32f as_gpu;
+
+    std::vector<float> gpu_sorted(n);
+    gpu::gpu_mem_32f as_gpu, bs_gpu;
     as_gpu.resizeN(n);
+    bs_gpu.resizeN(n);
+    auto in_gpu = &as_gpu;
+    auto out_gpu = &bs_gpu;
     {
         ocl::Kernel merge(merge_kernel, merge_kernel_length, "merge");
         merge.compile();
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            as_gpu.writeN(as.data(), n);
+            (*in_gpu).writeN(as.data(), n);
             t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфера данных
-            unsigned int workGroupSize = 128;
-            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            merge.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, n);
+
+            for(uint step = 0; 1<<step < n; step++) {
+                merge.exec(gpu::WorkSize(1, as.size()), *in_gpu, *out_gpu, step);
+                std::swap(in_gpu, out_gpu);
+            }
+
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
-        as_gpu.readN(as.data(), n);
+        std::cout << "GPU: " << ((float)n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
+        (*in_gpu).readN(gpu_sorted.data(), n);
     }
+
     // Проверяем корректность результатов
     for (int i = 0; i < n; ++i) {
-        EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
+        // std::cout << "  " << as[i] << " " << gpu_sorted[i] << " " << cpu_sorted[i] << "\n";
+        EXPECT_THE_SAME(gpu_sorted[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
