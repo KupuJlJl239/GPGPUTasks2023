3c77b411e24beda1c83f630e5690ce5649eae100
Kirill Sokolov <kirill141103@yandex.ru>
2023-10-21 23:21:54 +0300

    Сделал bitonic sort

--- src/cl/bitonic.cl
+++ src/cl/bitonic.cl
@@ -1,3 +1,30 @@
-__kernel void bitonic(__global float *as) {
-    // TODO
+#ifdef __CLION_IDE__
+    #include <libgpu/opencl/cl/clion_defines.cl>
+    #include <libgpu/opencl/cl/common.cl>
+#endif
+
+#line 6
+
+#define uint unsigned int
+
+__kernel void bitonic(__global float* arr, uint n, uint epoch, uint step)
+{
+    uint gid = get_global_id(0);
+    uint rank = epoch - step;
+
+    char reverse = (gid>>epoch) & 1;
+    uint i = ((gid >> rank) << (rank+1)) + (gid % (1<<rank));  // insert bit 0 to position $rank$ in $gid$
+    uint j = i ^ (1<<rank); // insert bit 1 to position $rank$ in $gid$
+
+    if(n <= i || n <= j)
+        return;
+
+    float a = arr[i];
+    float b = arr[j];
+
+    char should_swap = (a > b) ^ reverse;
+    if(should_swap){
+        arr[i] = b;
+        arr[j] = a;
+    }
 }
--- src/main_bitonic.cpp
+++ src/main_bitonic.cpp
@@ -11,6 +11,8 @@
 #include <stdexcept>
 #include <vector>
 
+using uint = unsigned int;
+
 
 template<typename T>
 void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line) {
@@ -31,12 +33,14 @@ int main(int argc, char **argv) {
     context.activate();
 
     int benchmarkingIters = 10;
-    unsigned int n = 32 * 1024 * 1024;
+    unsigned int n = 1024*1024;
     std::vector<float> as(n, 0);
     FastRandom r(n);
     for (unsigned int i = 0; i < n; ++i) {
         as[i] = r.nextf();
     }
+//    std::vector<float> as = {1, 0, 5, 3, 4, 2};
+//    uint n = as.size();
     std::cout << "Data generated for n=" << n << "!" << std::endl;
 
     std::vector<float> cpu_sorted;
@@ -48,26 +52,39 @@ int main(int argc, char **argv) {
             t.nextLap();
         }
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "CPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "CPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
     }
-    /*
+
     gpu::gpu_mem_32f as_gpu;
     as_gpu.resizeN(n);
 
     {
+        int log_n = 1;
+        while((1<<log_n) < n)
+            log_n += 1;
+        auto work_size = gpu::WorkSize(128, 1 << (log_n - 1));
+
         ocl::Kernel bitonic(bitonic_kernel, bitonic_kernel_length, "bitonic");
         bitonic.compile();
 
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             as_gpu.writeN(as.data(), n);
-
             t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфер данных
 
-            // TODO
+            for(uint epoch = 0; epoch < log_n; epoch++)
+                for(uint step = 0; step <= epoch; step++) {
+                    bitonic.exec(work_size, as_gpu, n, epoch, step);
+//                    as_gpu.readN(as.data(), n);
+//                    for(auto a: as)
+//                        std::cout << a << " ";
+//                    std::cout << std::endl;
+                }
+
+            t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "GPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
 
         as_gpu.readN(as.data(), n);
     }
@@ -76,6 +93,6 @@ int main(int argc, char **argv) {
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
